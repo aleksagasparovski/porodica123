40cb74be3291647668b70620e1af47aaa9768849
aleksagasparovski <aleksagasparovski@gmail.com>
2024-12-07 13:06:53 +0100

    porodica123

---
+++ porodica.py
@@ -0,0 +1,7 @@
+godina=2024
+mama=int(input("Koje je mamino godiste rodjenja"))
+dete1=int(input("Koliko 1. dete ima godina"))
+dete2=int(input("dete ima godina"))
+mamagodine=godina-mama
+print("kad je mama rodila prvo dete imala je",mamagodine-dete1)
+print("kad je mama rodila drugo dete imala je",mamagodine-dete2)
